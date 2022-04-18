884769b040959bd7660bee498d034d66d2e1c77a
amarees <saleh.amareen@dominos.com>
2022-04-17 21:48:53 -0400

    Added different profiled flatmap methods

--- src/main/java/com/javaetmoi/benchmark/MapperBenchmark.java
+++ src/main/java/com/javaetmoi/benchmark/MapperBenchmark.java
@@ -1,23 +1,15 @@
 package com.javaetmoi.benchmark;
 
-import com.javaetmoi.benchmark.mapping.mapper.OrderMapper;
-import com.javaetmoi.benchmark.mapping.mapper.bull.BullMapper;
-import com.javaetmoi.benchmark.mapping.mapper.datus.DatusMapper;
-import com.javaetmoi.benchmark.mapping.mapper.jmapper.JMapperMapper;
-import com.javaetmoi.benchmark.mapping.mapper.manual.ManualMapper;
-import com.javaetmoi.benchmark.mapping.mapper.mapstruct.MapStructMapper;
-import com.javaetmoi.benchmark.mapping.mapper.modelmapper.ModelMapper;
-import com.javaetmoi.benchmark.mapping.mapper.orika.OrikaMapper;
-import com.javaetmoi.benchmark.mapping.mapper.remappe.ReMappeMapper;
-import com.javaetmoi.benchmark.mapping.mapper.selma.SelmaMapper;
-import com.javaetmoi.benchmark.mapping.model.dto.OrderDTO;
+import com.javaetmoi.benchmark.mapping.model.entity.Address;
+import com.javaetmoi.benchmark.mapping.model.entity.AlphaCode2;
+import com.javaetmoi.benchmark.mapping.model.entity.Country;
+import com.javaetmoi.benchmark.mapping.model.entity.Customer;
+import com.javaetmoi.benchmark.mapping.model.entity.IsoCode;
 import com.javaetmoi.benchmark.mapping.model.entity.Order;
 import com.javaetmoi.benchmark.mapping.model.entity.OrderFactory;
 import java.util.Collection;
 import optional4j.spec.Optional;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -31,79 +23,57 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 public class MapperBenchmark {
 
-    @Param({"Manual", "MapStruct", "Selma", "JMapper", "datus", "Orika", "ModelMapper", "BULL", "Dozer", "ReMap"})
-    private String type;
-
-    private OrderMapper mapper;
-
     private final Order order = OrderFactory.buildOrder();
 
-    @Setup(Level.Trial)
+    @Setup
     public void setup() {
-        switch (type) {
-            case "Dozer":
-//                mapper = new DozerMapper();
-                break;
-            case "Orika":
-                mapper = new OrikaMapper();
-                break;
-            case "ModelMapper":
-                mapper = new ModelMapper();
-                break;
-            case "MapStruct":
-                mapper = new MapStructMapper();
-                break;
-            case "Selma":
-                mapper = new SelmaMapper();
-                break;
-            case "JMapper":
-                mapper = new JMapperMapper();
-                break;
-            case "Manual":
-                mapper = new ManualMapper();
-                break;
-            case "BULL":
-                mapper = new BullMapper();
-                break;
-            case "datus":
-                mapper = new DatusMapper();
-                break;
-            case "ReMap":
-                mapper = new ReMappeMapper();
-                break;
-            default:
-                throw new IllegalStateException("Unknown type: " + type);
-        }
-
-        mapper.map(order);
-
         for (int i = 0; i < 1000; i++) {
             Optional.ofNullable(null);
         }
     }
 
     @Benchmark
-    public OrderDTO mapper() {
-        return mapper.map(order);
+    public String toCustomerName() {
+        return order.getCustomer().map(Customer::getName).orElse(null);
+    }
+
+    @Benchmark
+    public String toShippingStreetAddress() {
+        return order.getCustomer().flatMap(Customer::getShippingAddress).map(Address::getStreet).orElse(null);
+    }
+
+    @Benchmark
+    public String toShippingCity() {
+        return order.getCustomer().flatMap(Customer::getShippingAddress).map(Address::getCity).orElse(null);
+    }
+
+    @Benchmark
+    public String toShippingAlphaCode2() {
+        return order.getCustomer().flatMap(Customer::getShippingAddress).flatMap(Address::getCountry).flatMap(Country::getIsoCode).flatMap(IsoCode::getAlphaCode2).map(AlphaCode2::getCode).orElse(null);
+    }
+
+    @Benchmark
+    public String toBillingStreetAddress() {
+        return order.getCustomer().flatMap(Customer::getBillingAddress).map(Address::getStreet).orElse(null);
+    }
+
+    @Benchmark
+    public String toBillingCity() {
+        return order.getCustomer().flatMap(Customer::getBillingAddress).map(Address::getCity).orElse(null);
+    }
+
+    @Benchmark
+    public String toBillingAlphaCode2() {
+        return order.getCustomer().flatMap(Customer::getBillingAddress).flatMap(Address::getCountry).flatMap(Country::getIsoCode).flatMap(IsoCode::getAlphaCode2).map(AlphaCode2::getCode).orElse(null);
     }
 
     public static void main(String... args) throws Exception {
-        Options opts = new OptionsBuilder()
-                .include(MapperBenchmark.class.getSimpleName())
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .jvmArgs("-server")
-                .forks(1)
-                .resultFormat(ResultFormatType.TEXT)
-                .build();
+        Options opts = new OptionsBuilder().include(MapperBenchmark.class.getSimpleName()).warmupIterations(5).measurementIterations(5).jvmArgs("-server").forks(1).resultFormat(ResultFormatType.TEXT).build();
 
         Collection<RunResult> results = new Runner(opts).run();
         for (RunResult result : results) {
             Result<?> r = result.getPrimaryResult();
-            System.out.println("API replied benchmark score: "
-                    + r.getScore() + " "
-                    + r.getScoreUnit() + " over "
-                    + r.getStatistics().getN() + " iterations");
+            System.out.println("API replied benchmark score: " + r.getScore() + " " + r.getScoreUnit() + " over " + r.getStatistics().getN() + " iterations");
         }
     }
 }
